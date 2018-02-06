b55ac327c1e9b6bc543252dd7b20a12d0b9e637b
Dhaval Savalia <dhaval.savalia6@gmail.com>
2018-02-06 19:47:16 +0530

    use split() rather than regex/slugify

--- scrap_movie.py
+++ scrap_movie.py
@@ -1,27 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 with open('links.html') as fp:
     soup = BeautifulSoup(fp, "html.parser")
 
-# for link in soup.find_all('a'):
-r = requests.get("https://www.clevelandfilm.org/films/2017/zip-zap-and-the-captains-island")
-url = "https://www.clevelandfilm.org/films/2017/zip-zap-and-the-captains-island"
-url = url.split("https://www.clevelandfilm.org/films/2017/")
-movie_soup = BeautifulSoup(r.content, "html.parser")
-main_content = movie_soup.find('div', {'id': 'ciff-left'})
-title = main_content.find('h1', {'itemprop': 'name'})
-f = open("{}.html".format(url[1]),"w+") # line where problem lies
-info = main_content.find('p', {'class': 'info'})
-country_meta = info.find('span', {'itemprop': 'countryOfOrigin'})
-country = country_meta.find('span', {'itemprop': 'name'})
-runtime = info.find('span', {'itemprop': 'duration'})
-description_meta = main_content.find('span', {'itemprop': 'description'})
-description = description_meta.find('p')
-write_data = '''<h1>{}</h1>    
-<p>Country: {}</p>
-<p>Runtime: {}</p>
-<p>Description: {}</p>'''.format(title.text, country.text, runtime.text, description.text)
-f.write(write_data)
-f.close()
+for link in soup.find_all('a'):
+    url = link.get('href')
+    url = url.split("https://www.clevelandfilm.org/films/2017/")
+    r = requests.get(link.get('href'))
+    movie_soup = BeautifulSoup(r.content, "html.parser")
+    main_content = movie_soup.find('div', {'id': 'ciff-left'})
+    title = main_content.find('h1', {'itemprop': 'name'})
+    f = open("{}.html".format(url[1]),"w+")
+    info = main_content.find('p', {'class': 'info'})
+    country_meta = info.find('span', {'itemprop': 'countryOfOrigin'})
+    country = country_meta.find('span', {'itemprop': 'name'})
+    runtime = info.find('span', {'itemprop': 'duration'})
+    description_meta = main_content.find('span', {'itemprop': 'description'})
+    description = description_meta.find('p')
+    write_data = '''<h1>{}</h1>
+    <p>Country: {}</p>
+    <p>Runtime: {}</p>
+    <p>Description: {}</p>'''.format(title.text, country.text, runtime.text, description.text)
+    f.write(write_data)
+    f.close()
